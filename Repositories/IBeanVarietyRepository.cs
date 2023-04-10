8d8c575400a8b9afaea59b6d538f02a4c9ca587a
Pritesh Zaver <priteshzaver@gmail.com>
2023-04-10 12:14:02 -0500

    initial commit

---
+++ Repositories/IBeanVarietyRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Repositories
+{
+    public interface IBeanVarietyRepository
+    {
+        void Add(BeanVariety variety);
+        void Delete(int id);
+        BeanVariety Get(int id);
+        List<BeanVariety> GetAll();
+        void Update(BeanVariety variety);
+    }
+}
