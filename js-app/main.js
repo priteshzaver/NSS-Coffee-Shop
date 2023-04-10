2864d29c3317cba6660b5b3067fd2c50f0a7ac9d
Pritesh Zaver <priteshzaver@gmail.com>
2023-04-10 15:49:55 -0500

    displayed to the dom

--- js-app/main.js
+++ js-app/main.js
@@ -3,11 +3,24 @@ const url = "https://localhost:5001/api/beanvariety/";
 const button = document.querySelector("#run-button");
 button.addEventListener("click", () => {
     getAllBeanVarieties()
-        .then(beanVarieties => {
-            console.log(beanVarieties);
+        .then(beans => {
+            beans.map(bean => {
+
+                const newDiv = document.createElement("div");
+                
+                const newContent = document.createTextNode(`${bean.name}`);
+                
+                newDiv.appendChild(newContent);
+                
+                const currentDiv = document.getElementById("coffee");
+                document.body.insertBefore(newDiv, currentDiv);
+            })
         })
+
+
 });
 
 function getAllBeanVarieties() {
     return fetch(url).then(resp => resp.json());
 }
+
